469b207ad4ae2005bfa6d5ace38886a6abc31000
Anna Cho <loveablessing@gmail.com>
2022-06-02 18:31:29 -0400

    commit tree 403cd7e0a7f24762559645a5917f48c4b3972131

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(array, string){
+    const obj = array.filter(element => element.toUpperCase() === string.toUpperCase())
+    return obj;
+
+}
+function fuzzyMatch(array, string){
+    const fuzz = array.filter(element => element[0] === string[0])
+    return fuzz;
+}
+function matchName(driver, string){
+    const matchName = driver.filter(element => element.name === string)
+    return matchName;
+}
+findMatching(drivers, 'Bobby')
